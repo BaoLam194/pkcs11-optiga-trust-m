2ebe8cd014cb80af91b72615f43c16602b7ec1e3
lames <135998293+BaoLam194@users.noreply.github.com>
2026-09-24 14:19:04 +0800

    Initialize nginx server with trust m dir

---
+++ tests/nginx/README.md
@@ -0,0 +1,7 @@
+<!--
+SPDX-FileCopyrightText: 2024 Infineon Technologies AG
+
+SPDX-License-Identifier: MIT
+-->
+
+# This folder is for showcasing the Nginx server with Optiga Trust M certificate as server certificate.
---
+++ tests/nginx/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2026 Infineon Technologies AG
+#
+# SPDX-License-Identifier: MIT
+
+chmod +x pd
+dos2unix pd
+
+set -e
+
+echo "SSL/TLS Nginx Server with Trust M"
+echo "=================================================="
+
+
+echo "=================================================="
+echo "Retrieve the public cert from Trust M"
+echo "=================================================="
+./pd --slot 0 --label Cert --read-object --type cert --output-file temp_server_cert.der
+
+openssl x509 -in temp_server_cert.der -outform PEM -out server_cert_TrustM.pem
+mv server_cert_TrustM.pem certificates/server_cert_TrustM.pem
+sudo cp default /etc/nginx/sites-enabled/default
+sudo cp certificates/server_cert_TrustM.pem /etc/nginx/server_cert_TrustM.pem
+sudo service nginx restart
